12dafc2fc8749556b3cc99bb5eda63ab895e3033
Joana Elise <joanaelise@gmail.com>
2025-06-21 19:31:03 -0300

    atualizado o cardapio e contato

--- src/app/pages/contato/contato.ts
+++ src/app/pages/contato/contato.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contato.html',
-  styleUrl: './contato.css'
+  styleUrls: ['./contato.css']
 })
 export class Contato {
-
+  nome = '';
+  telefone = '';
+  data = 'hoje';
+  horario = '';
+  pessoas = '';
 }
